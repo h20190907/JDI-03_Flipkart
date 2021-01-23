000400db9e3b60c8600869c6102b08b1e96ecc24
Dilpreet kaur <dilpreetkaur@Dilpreets-MacBook-Pro.local>
2021-01-23 14:54:25 +0530

    added constant file for sql query

---
+++ CRS-JEDI-Flipkart/src/com/flipkart/constant/SQLQueriesConstant.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.flipkart.constant;
+
+/**
+ * @author dilpreetkaur
+ *
+ */
+public class SQLQueriesConstant {
+	public static final String VIEW_PROFESSOR_QUERY="select";
+
+}
