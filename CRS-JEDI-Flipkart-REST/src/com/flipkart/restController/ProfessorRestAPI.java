01ca045ad895a00ca7c66c7fb70250651fb3e96a
Dilpreet kaur <dilpreetkaur@Dilpreets-MacBook-Pro.local>
2021-01-29 11:11:17 +0530

    logger changes

--- CRS-JEDI-Flipkart-REST/src/com/flipkart/restController/ProfessorRestAPI.java
+++ CRS-JEDI-Flipkart-REST/src/com/flipkart/restController/ProfessorRestAPI.java
@@ -2,11 +2,18 @@ package com.flipkart.restController;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
 import com.flipkart.bean.Course;
 import com.flipkart.bean.EnrolledStudent;
 import com.flipkart.service.ProfessorInterface;
 import com.flipkart.service.ProfessorOperation;
 
+@Path("/professor")
 public class ProfessorRestAPI {
 	ProfessorInterface professorInterface=ProfessorOperation.getInstance();
 	public List<EnrolledStudent> viewEnrolledStudents(String profId)
@@ -23,6 +30,9 @@ public class ProfessorRestAPI {
 		return students;
 	}
 	
+	@GET
+	@Path("/getCourses")
+	@Produces(MediaType.APPLICATION_JSON)
 	public List<Course> getCourses(String profId)
 	{
 		List<Course> courses=new ArrayList<Course>();
