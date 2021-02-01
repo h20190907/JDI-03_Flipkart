b806744ce0715eebfadb902b2947499814442cb7
Dilpreet kaur <dilpreetkaur@Dilpreets-MacBook-Pro.local>
2021-02-01 09:23:21 +0530

    user resources added

--- CRS-JEDI-Flipkart-REST/src/com/flipkart/restController/ProfessorRestAPI.java
+++ CRS-JEDI-Flipkart-REST/src/com/flipkart/restController/ProfessorRestAPI.java
@@ -10,11 +10,13 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import com.flipkart.bean.Course;
 import com.flipkart.bean.EnrolledStudent;
 import com.flipkart.service.ProfessorInterface;
 import com.flipkart.service.ProfessorOperation;
+import com.flipkart.validator.ProfessorValidator;
 
 @Path("/professor")
 public class ProfessorRestAPI {
@@ -58,8 +60,29 @@ public class ProfessorRestAPI {
 	@POST
 	@Path("/addGrade")
 	@Consumes(MediaType.APPLICATION_JSON)
-	public void addGrade()
+	public Response addGrade(@QueryParam("studentId") int studentId,@QueryParam("courseCode") String courseCode,@QueryParam("profId") String profId,@QueryParam("grade") String grade)
 	{
+		try
+		{
+			List<EnrolledStudent> enrolledStudents=new ArrayList<EnrolledStudent>();
+			enrolledStudents=professorInterface.viewEnrolledStudents(profId);
+			List<Course> coursesEnrolled=new ArrayList<Course>();
+			coursesEnrolled	=professorInterface.getCourses(profId);
+			if(ProfessorValidator.isValidStudent(enrolledStudents, studentId) && ProfessorValidator.isValidCourse(coursesEnrolled, courseCode))
+			{
+				professorInterface.addGrade(studentId, courseCode, grade);
+			}
+			else
+			{
+				//error code
+				return Response.status(500).entity( "Something went wrong, Please Try Again ! ").build();
+			}
+		}
+		catch(Exception ex)
+		{
+			return Response.status(500).entity( "Something went wrong, Please Try Again ! ").build();
+		}
+		return Response.status(200).entity( "Grade updated for student: "+studentId).build();
 		
 	}
 	
