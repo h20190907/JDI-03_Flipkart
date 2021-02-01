f35093590fa2c679866014c4bd3e16da9ee550c6
u-square2 <umangughareja4121@gmail.com>
2021-02-02 01:06:27 +0530

    comments added

--- CRS-JEDI-Flipkart-REST/src/com/flipkart/bean/Admin.java
+++ CRS-JEDI-Flipkart-REST/src/com/flipkart/bean/Admin.java
@@ -11,10 +11,18 @@ import java.util.Date;
 public class Admin extends User{
 	private Date dateOfJoining;
 
+	/**
+	 * Method to get Date of joining
+	 * @return Date of joining
+	 */
 	public Date getDateOfJoining() {
 		return dateOfJoining;
 	}
 
+	/**
+	 * Method to set Date of joining
+	 * @param dateOfJoining
+	 */
 	public void setDateOfJoining(Date dateOfJoining) {
 		this.dateOfJoining = dateOfJoining;
 	}
