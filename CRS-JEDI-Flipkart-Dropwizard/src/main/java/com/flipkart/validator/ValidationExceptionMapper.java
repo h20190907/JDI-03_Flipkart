9b508ee9f21b055bd370d3b480b68e164d24cfa1
Anurag Behera <anurag.behera@BLRETV-C02DG6E0.local>
2021-02-03 03:08:10 +0530

    Comments added to validator

--- CRS-JEDI-Flipkart-Dropwizard/src/main/java/com/flipkart/validator/ValidationExceptionMapper.java
+++ CRS-JEDI-Flipkart-Dropwizard/src/main/java/com/flipkart/validator/ValidationExceptionMapper.java
@@ -7,20 +7,30 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
+/**
+ * 
+ * @author JEDI-03
+ * Class for Validation Exception Mapper
+ *
+ */
 @Provider
 public class ValidationExceptionMapper implements ExceptionMapper<javax.validation.ValidationException> {
 
- @Override
- public Response toResponse(javax.validation.ValidationException e) {
-     final StringBuilder strBuilder = new StringBuilder();
-     for (ConstraintViolation<?> cv : ((ConstraintViolationException) e).getConstraintViolations()) {
-         strBuilder.append(cv.getPropertyPath().toString() + " " + cv.getMessage());
-     }
-     return Response
-             .status(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode())
-             .type(MediaType.TEXT_PLAIN)
-             .entity(strBuilder.toString())
-             .build();
- }
+	/**
+	 * @param exception
+	 * @return Response
+	 */
+	 @Override
+	 public Response toResponse(javax.validation.ValidationException e) {
+	     final StringBuilder strBuilder = new StringBuilder();
+	     for (ConstraintViolation<?> cv : ((ConstraintViolationException) e).getConstraintViolations()) {
+	         strBuilder.append(cv.getPropertyPath().toString() + " " + cv.getMessage());
+	     }
+	     return Response
+	             .status(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode())
+	             .type(MediaType.TEXT_PLAIN)
+	             .entity(strBuilder.toString())
+	             .build();
+	 }
 }
  
